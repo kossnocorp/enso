d7946cec2d8eea024a57a939d553784c0de424be
Sasha Koss <koss@nocorp.me>
2025-01-10 09:38:15 +0800

    Reorganize browser Form tests

--- src/form/tests.tsx
+++ src/form/tests.tsx
@@ -9,304 +9,314 @@ import { FieldRef } from "../field/ref/index.ts";
 import { Form } from "./index.tsx";
 
 describe("Form", () => {
-  it("allows to handle submit", async () => {
-    const spy = vi.fn();
-    let resolveSubmit: ((value: unknown) => void) | undefined;
-    const submitPromise = new Promise((resolve) => {
-      resolveSubmit = resolve;
+  describe("control", () => {
+    describe("control", () => {
+      it("allows to handle submit", async () => {
+        const spy = vi.fn();
+        let resolveSubmit: ((value: unknown) => void) | undefined;
+        const submitPromise = new Promise((resolve) => {
+          resolveSubmit = resolve;
+        });
+
+        function Component() {
+          const count = useRenderCount();
+          const form = Form.use({ hello: "world" });
+
+          return (
+            <div>
+              <div data-testid="render-submit">{count}</div>
+
+              <button onClick={() => form.$.hello.set("Sasha")}>
+                Update field
+              </button>
+
+              <form
+                {...form.control({
+                  onSubmit: (values) => {
+                    spy(values);
+                    return submitPromise;
+                  },
+                })}
+              >
+                <div data-testid="submitting">{String(form.submitting)}</div>
+
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+          );
+        }
+
+        const screen = render(<Component />);
+
+        await expect
+          .element(screen.getByTestId("submitting"))
+          .toHaveTextContent("false");
+
+        await screen.getByText("Update field").click();
+
+        await expect
+          .element(screen.getByTestId("render-submit"))
+          .toHaveTextContent("1");
+
+        expect(spy).not.toBeCalled();
+
+        await screen.getByText("Submit").click();
+
+        expect(spy).toBeCalledWith({
+          hello: "Sasha",
+        });
+
+        await expect
+          .element(screen.getByTestId("submitting"))
+          .toHaveTextContent("true");
+
+        await expect
+          .element(screen.getByTestId("render-submit"))
+          .toHaveTextContent("2");
+
+        resolveSubmit?.(void 0);
+
+        await expect
+          .element(screen.getByTestId("submitting"))
+          .toHaveTextContent("false");
+
+        await expect
+          .element(screen.getByTestId("render-submit"))
+          .toHaveTextContent("3");
+      });
     });
 
-    function Component() {
-      const count = useRenderCount();
-      const form = Form.use({ hello: "world" });
+    describe("Control", () => {
+      it("allows to use Form component", async () => {
+        const spy = vi.fn();
+        let resolveSubmit: ((value: unknown) => void) | undefined;
+        const submitPromise = new Promise((resolve) => {
+          resolveSubmit = resolve;
+        });
 
-      return (
-        <div>
-          <div data-testid="render-submit">{count}</div>
+        function Component() {
+          const count = useRenderCount();
+          const form = Form.use({ hello: "world" });
 
-          <button onClick={() => form.$.hello.set("Sasha")}>
-            Update field
-          </button>
+          return (
+            <div>
+              <div data-testid="render-submit">{count}</div>
 
-          <form
-            {...form.control((values) => {
-              spy(values);
-              return submitPromise;
-            })}
-          >
-            <div data-testid="submitting">{String(form.submitting)}</div>
+              <button onClick={() => form.$.hello.set("Sasha")}>
+                Update field
+              </button>
 
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-      );
-    }
+              <form.Control
+                onSubmit={(values) => {
+                  spy(values);
+                  return submitPromise;
+                }}
+              >
+                <div data-testid="submitting">{String(form.submitting)}</div>
 
-    const screen = render(<Component />);
+                <input {...form.$.hello.input()} data-testid="hello-input" />
 
-    await expect
-      .element(screen.getByTestId("submitting"))
-      .toHaveTextContent("false");
+                <button type="submit">Submit</button>
+              </form.Control>
+            </div>
+          );
+        }
 
-    await screen.getByText("Update field").click();
+        const screen = render(<Component />);
 
-    await expect
-      .element(screen.getByTestId("render-submit"))
-      .toHaveTextContent("1");
+        await expect
+          .element(screen.getByTestId("submitting"))
+          .toHaveTextContent("false");
 
-    expect(spy).not.toBeCalled();
+        await userEvent.fill(screen.getByTestId("hello-input"), "Sasha");
 
-    await screen.getByText("Submit").click();
+        await expect
+          .element(screen.getByTestId("render-submit"))
+          .toHaveTextContent("1");
 
-    expect(spy).toBeCalledWith({
-      hello: "Sasha",
+        expect(spy).not.toBeCalled();
+
+        await screen.getByText("Submit").click();
+
+        expect(spy).toBeCalledWith({
+          hello: "Sasha",
+        });
+
+        await expect
+          .element(screen.getByTestId("submitting"))
+          .toHaveTextContent("true");
+
+        await expect
+          .element(screen.getByTestId("render-submit"))
+          .toHaveTextContent("2");
+
+        resolveSubmit?.(void 0);
+
+        await expect
+          .element(screen.getByTestId("submitting"))
+          .toHaveTextContent("false");
+
+        await expect
+          .element(screen.getByTestId("render-submit"))
+          .toHaveTextContent("3");
+      });
+
+      it("assigns form id", async () => {
+        let formId: string | undefined;
+
+        function Component() {
+          const form = Form.use({ hello: "world" });
+          formId = form.id;
+          return <form.Control data-testid="form" onSubmit={() => {}} />;
+        }
+
+        const screen = render(<Component />);
+
+        expect(formId).toBeTypeOf("string");
+
+        await expect
+          .element(screen.getByTestId("form"))
+          .toHaveAttribute("id", formId);
+      });
+    });
+  });
+
+  describe("validation", () => {
+    it("validates form on submit", async () => {
+      const spy = vi.fn();
+
+      function Component() {
+        const count = useRenderCount();
+        const form = Form.use({ hello: "world" }, { validate: validateHello });
+
+        return (
+          <div>
+            <div data-testid="render-validate">{count}</div>
+
+            <button onClick={() => form.$.hello.set("Sasha")}>Set hello</button>
+
+            <form.Control onSubmit={spy}>
+              <form.$.hello.Control
+                error
+                render={(control, { error }) => {
+                  return (
+                    <div>
+                      <input
+                        {...control}
+                        onChange={(e) => {
+                          control.onChange(e.target.value);
+                        }}
+                        data-testid="hello-input"
+                      />
+
+                      {error && <div data-testid="error">{error.message}</div>}
+                    </div>
+                  );
+                }}
+              />
+
+              <button type="submit">Submit</button>
+            </form.Control>
+          </div>
+        );
+      }
+
+      const screen = render(<Component />);
+
+      await expect.element(screen.getByTestId("error")).not.toBeInTheDocument();
+
+      await userEvent.fill(screen.getByTestId("hello-input"), "");
+
+      await expect
+        .element(screen.getByTestId("render-validate"))
+        .toHaveTextContent("1");
+
+      await screen.getByText("Submit").click();
+
+      expect(spy).not.toBeCalled();
+
+      await expect
+        .element(screen.getByTestId("error"))
+        .toHaveTextContent("Hello is required");
+
+      await expect
+        .element(screen.getByTestId("render-validate"))
+        .toHaveTextContent("2");
+
+      await screen.getByText("Set hello").click();
+
+      await expect
+        .element(screen.getByTestId("render-validate"))
+        .toHaveTextContent("2");
+
+      await screen.getByText("Submit").click();
+
+      await expect.element(screen.getByTestId("error")).not.toBeInTheDocument();
+
+      expect(spy).toBeCalledWith({ hello: "Sasha" });
+
+      await expect
+        .element(screen.getByTestId("render-validate"))
+        .toHaveTextContent("4");
     });
 
-    await expect
-      .element(screen.getByTestId("submitting"))
-      .toHaveTextContent("true");
+    it("revalidates form on fields blur", async () => {
+      const spy = vi.fn();
 
-    await expect
-      .element(screen.getByTestId("render-submit"))
-      .toHaveTextContent("2");
+      function Component() {
+        const count = useRenderCount();
+        const form = Form.use({ hello: "world" }, { validate: validateHello });
 
-    resolveSubmit?.(void 0);
+        return (
+          <div>
+            <div data-testid="render-validate">{count}</div>
 
-    await expect
-      .element(screen.getByTestId("submitting"))
-      .toHaveTextContent("false");
+            <form.Control onSubmit={spy}>
+              <form.$.hello.Control
+                error
+                render={(control, { error }) => {
+                  return (
+                    <div>
+                      <input
+                        {...control}
+                        onChange={(e) => {
+                          control.onChange(e.target.value);
+                        }}
+                        data-testid="hello-input"
+                      />
 
-    await expect
-      .element(screen.getByTestId("render-submit"))
-      .toHaveTextContent("3");
-  });
+                      {error && <div data-testid="error">{error.message}</div>}
+                    </div>
+                  );
+                }}
+              />
 
-  it("allows to use Form component", async () => {
-    const spy = vi.fn();
-    let resolveSubmit: ((value: unknown) => void) | undefined;
-    const submitPromise = new Promise((resolve) => {
-      resolveSubmit = resolve;
+              <button type="submit">Submit</button>
+            </form.Control>
+          </div>
+        );
+      }
+
+      const screen = render(<Component />);
+
+      await expect.element(screen.getByTestId("error")).not.toBeInTheDocument();
+
+      await userEvent.fill(screen.getByTestId("hello-input"), "");
+      (screen.getByTestId("hello-input").element() as HTMLInputElement).blur();
+
+      await screen.getByText("Submit").click();
+
+      expect(spy).not.toBeCalled();
+
+      await expect
+        .element(screen.getByTestId("error"))
+        .toHaveTextContent("Hello is required");
+
+      await userEvent.fill(screen.getByTestId("hello-input"), "Sasha");
+      (screen.getByTestId("hello-input").element() as HTMLInputElement).blur();
+
+      await expect.element(screen.getByTestId("error")).not.toBeInTheDocument();
     });
-
-    function Component() {
-      const count = useRenderCount();
-      const form = Form.use({ hello: "world" });
-
-      return (
-        <div>
-          <div data-testid="render-submit">{count}</div>
-
-          <button onClick={() => form.$.hello.set("Sasha")}>
-            Update field
-          </button>
-
-          <form.Control
-            onSubmit={(values) => {
-              spy(values);
-              return submitPromise;
-            }}
-          >
-            <div data-testid="submitting">{String(form.submitting)}</div>
-
-            <input {...form.$.hello.input()} data-testid="hello-input" />
-
-            <button type="submit">Submit</button>
-          </form.Control>
-        </div>
-      );
-    }
-
-    const screen = render(<Component />);
-
-    await expect
-      .element(screen.getByTestId("submitting"))
-      .toHaveTextContent("false");
-
-    await userEvent.fill(screen.getByTestId("hello-input"), "Sasha");
-
-    await expect
-      .element(screen.getByTestId("render-submit"))
-      .toHaveTextContent("1");
-
-    expect(spy).not.toBeCalled();
-
-    await screen.getByText("Submit").click();
-
-    expect(spy).toBeCalledWith({
-      hello: "Sasha",
-    });
-
-    await expect
-      .element(screen.getByTestId("submitting"))
-      .toHaveTextContent("true");
-
-    await expect
-      .element(screen.getByTestId("render-submit"))
-      .toHaveTextContent("2");
-
-    resolveSubmit?.(void 0);
-
-    await expect
-      .element(screen.getByTestId("submitting"))
-      .toHaveTextContent("false");
-
-    await expect
-      .element(screen.getByTestId("render-submit"))
-      .toHaveTextContent("3");
-  });
-
-  it("assigns form id", async () => {
-    let formId: string | undefined;
-
-    function Component() {
-      const form = Form.use({ hello: "world" });
-      formId = form.id;
-      return <form.Control data-testid="form" onSubmit={() => {}} />;
-    }
-
-    const screen = render(<Component />);
-
-    expect(formId).toBeTypeOf("string");
-
-    await expect
-      .element(screen.getByTestId("form"))
-      .toHaveAttribute("id", formId);
-  });
-
-  it("validates form on submit", async () => {
-    const spy = vi.fn();
-
-    function Component() {
-      const count = useRenderCount();
-      const form = Form.use({ hello: "world" }, { validate: validateHello });
-
-      return (
-        <div>
-          <div data-testid="render-validate">{count}</div>
-
-          <button onClick={() => form.$.hello.set("Sasha")}>Set hello</button>
-
-          <form.Control onSubmit={spy}>
-            <form.$.hello.Control
-              error
-              render={(control, { error }) => {
-                return (
-                  <div>
-                    <input
-                      {...control}
-                      onChange={(e) => {
-                        control.onChange(e.target.value);
-                      }}
-                      data-testid="hello-input"
-                    />
-
-                    {error && <div data-testid="error">{error.message}</div>}
-                  </div>
-                );
-              }}
-            />
-
-            <button type="submit">Submit</button>
-          </form.Control>
-        </div>
-      );
-    }
-
-    const screen = render(<Component />);
-
-    await expect.element(screen.getByTestId("error")).not.toBeInTheDocument();
-
-    await userEvent.fill(screen.getByTestId("hello-input"), "");
-
-    await expect
-      .element(screen.getByTestId("render-validate"))
-      .toHaveTextContent("1");
-
-    await screen.getByText("Submit").click();
-
-    expect(spy).not.toBeCalled();
-
-    await expect
-      .element(screen.getByTestId("error"))
-      .toHaveTextContent("Hello is required");
-
-    await expect
-      .element(screen.getByTestId("render-validate"))
-      .toHaveTextContent("2");
-
-    await screen.getByText("Set hello").click();
-
-    await expect
-      .element(screen.getByTestId("render-validate"))
-      .toHaveTextContent("2");
-
-    await screen.getByText("Submit").click();
-
-    await expect.element(screen.getByTestId("error")).not.toBeInTheDocument();
-
-    expect(spy).toBeCalledWith({ hello: "Sasha" });
-
-    await expect
-      .element(screen.getByTestId("render-validate"))
-      .toHaveTextContent("4");
-  });
-
-  it("revalidates form on fields blur", async () => {
-    const spy = vi.fn();
-
-    function Component() {
-      const count = useRenderCount();
-      const form = Form.use({ hello: "world" }, { validate: validateHello });
-
-      return (
-        <div>
-          <div data-testid="render-validate">{count}</div>
-
-          <form.Control onSubmit={spy}>
-            <form.$.hello.Control
-              error
-              render={(control, { error }) => {
-                return (
-                  <div>
-                    <input
-                      {...control}
-                      onChange={(e) => {
-                        control.onChange(e.target.value);
-                      }}
-                      data-testid="hello-input"
-                    />
-
-                    {error && <div data-testid="error">{error.message}</div>}
-                  </div>
-                );
-              }}
-            />
-
-            <button type="submit">Submit</button>
-          </form.Control>
-        </div>
-      );
-    }
-
-    const screen = render(<Component />);
-
-    await expect.element(screen.getByTestId("error")).not.toBeInTheDocument();
-
-    await userEvent.fill(screen.getByTestId("hello-input"), "");
-    (screen.getByTestId("hello-input").element() as HTMLInputElement).blur();
-
-    await screen.getByText("Submit").click();
-
-    expect(spy).not.toBeCalled();
-
-    await expect
-      .element(screen.getByTestId("error"))
-      .toHaveTextContent("Hello is required");
-
-    await userEvent.fill(screen.getByTestId("hello-input"), "Sasha");
-    (screen.getByTestId("hello-input").element() as HTMLInputElement).blur();
-
-    await expect.element(screen.getByTestId("error")).not.toBeInTheDocument();
   });
 });
 
