64f519695708871b976ebcb50b523940553901ec
Sasha Koss <koss@nocorp.me>
2025-08-04 08:24:48 +0000

    Restore browser tests, migrate form implementation

---
+++ src/field/ref/tests.ts
@@ -0,0 +1,237 @@
+import { describe, expect, it, vi } from "vitest";
+import { postpone } from "../../../tests/utils.ts";
+import { change } from "../../change/index.ts";
+import { FieldOld } from "../old.tsx";
+import { FieldRefOld, MaybeFieldRefOld } from "./old.ts";
+
+describe.skip(FieldRefOld, () => {
+  describe("tree", () => {
+    describe(FieldRefOld.prototype.maybe, () => {
+      it("returns MaybeFieldRef instance", () => {
+        const field = new FieldOld<string | number | undefined>(undefined);
+        const fieldRef = new FieldRefOld(field);
+        const maybeFieldRef = fieldRef.maybe();
+        maybeFieldRef satisfies MaybeFieldRefOld<string | number | undefined>;
+        // @ts-expect-error: It should not be any
+        maybeFieldRef satisfies MaybeFieldRefOld<bigint>;
+        expect(maybeFieldRef instanceof MaybeFieldRefOld).toBe(true);
+        expect(maybeFieldRef.get()).toBeUndefined();
+      });
+    });
+  });
+});
+
+describe.skip(MaybeFieldRefOld, () => {
+  describe("tree", () => {
+    describe("#at", () => {
+      describe("primitive", () => {
+        it("returns the undefined field as is if it's defined", () => {
+          const field = new FieldOld<string | number | undefined>(undefined);
+          const fieldRef = new FieldRefOld(field);
+          const maybeFieldRef = fieldRef.maybe();
+          maybeFieldRef satisfies MaybeFieldRefOld<string | number | undefined>;
+          // @ts-expect-error: It should not be any
+          maybeFieldRef satisfies MaybeFieldRefOld<bigint>;
+          expect(maybeFieldRef instanceof MaybeFieldRefOld).toBe(true);
+          expect(maybeFieldRef.get()).toBeUndefined();
+        });
+      });
+
+      describe("object", () => {
+        it("returns the undefined field as is if it's defined", () => {
+          const field = new FieldOld<{ a: string } | undefined>(undefined);
+          const fieldRef = new FieldRefOld(field);
+          const maybeFieldRef = fieldRef.maybe();
+          maybeFieldRef satisfies MaybeFieldRefOld<{ a: string } | undefined>;
+          // @ts-expect-error: It should not be any
+          maybeFieldRef satisfies MaybeFieldRefOld<bigint>;
+          expect(maybeFieldRef instanceof MaybeFieldRefOld).toBe(true);
+          expect(maybeFieldRef.get()).toBeUndefined();
+        });
+
+        it("allows to access properties by key", () => {
+          const field = new FieldOld<{ a?: string } | undefined>(undefined);
+          const fieldRef = new FieldRefOld(field);
+          const maybeFieldRef = fieldRef.maybe().at("a");
+          maybeFieldRef satisfies MaybeFieldRefOld<string | undefined>;
+          // @ts-expect-error: It should not be any
+          maybeFieldRef satisfies MaybeFieldRefOld<bigint>;
+          expect(maybeFieldRef instanceof MaybeFieldRefOld).toBe(true);
+          expect(maybeFieldRef.get()).toBeUndefined();
+        });
+
+        it("allows accessing maybe undefined properties", () => {
+          const field = new FieldOld<
+            { a?: { b?: { c?: number | string } } } | undefined
+          >(undefined);
+          const fieldRef = new FieldRefOld(field);
+          const maybeFieldRef = fieldRef.maybe().at("a").at("b").at("c");
+          maybeFieldRef satisfies MaybeFieldRefOld<number | string | undefined>;
+          // @ts-expect-error: It should not be any
+          maybeFieldRef satisfies MaybeFieldRefOld<bigint>;
+          expect(maybeFieldRef instanceof MaybeFieldRefOld).toBe(true);
+          expect(maybeFieldRef.get()).toBeUndefined();
+        });
+
+        it("resolves proper field for deep nested properties", () => {
+          const field = new FieldOld<
+            { a?: { b?: { c?: number | string } } } | undefined
+          >({ a: { b: { c: 123 } } });
+          const fieldRef = new FieldRefOld(field);
+          const maybeFieldRef = fieldRef.maybe().at("a").at("b").at("c");
+          maybeFieldRef satisfies MaybeFieldRefOld<number | string | undefined>;
+          // @ts-expect-error: It should not be any
+          maybeFieldRef satisfies MaybeFieldRefOld<bigint>;
+          expect(maybeFieldRef instanceof MaybeFieldRefOld).toBe(true);
+          expect(maybeFieldRef.get()).toBe(123);
+        });
+      });
+
+      describe("array", () => {
+        it("returns the undefined field as is if it's defined", () => {
+          const field = new FieldOld<string[] | undefined>(undefined);
+          const fieldRef = new FieldRefOld(field);
+          const maybeFieldRef = fieldRef.maybe();
+          maybeFieldRef satisfies MaybeFieldRefOld<string[] | undefined>;
+          // @ts-expect-error: It should not be any
+          maybeFieldRef satisfies MaybeFieldRefOld<bigint>;
+          expect(maybeFieldRef instanceof MaybeFieldRefOld).toBe(true);
+          expect(maybeFieldRef.get()).toBeUndefined();
+        });
+
+        it("allows to access items by index", () => {
+          const field = new FieldOld<string[] | undefined>(undefined);
+          const fieldRef = new FieldRefOld(field);
+          const maybeFieldRef = fieldRef.maybe().at(0);
+          maybeFieldRef satisfies MaybeFieldRefOld<string | undefined>;
+          // @ts-expect-error: It should not be any
+          maybeFieldRef satisfies MaybeFieldRefOld<bigint>;
+          expect(maybeFieldRef instanceof MaybeFieldRefOld).toBe(true);
+          expect(maybeFieldRef.get()).toBeUndefined();
+        });
+
+        it("allows accessing maybe undefined items", () => {
+          const field = new FieldOld<string[][][] | undefined>(undefined);
+          const fieldRef = new FieldRefOld(field);
+          const maybeFieldRef = fieldRef.maybe().at(0).at(0).at(0);
+          maybeFieldRef satisfies MaybeFieldRefOld<string | undefined>;
+          // @ts-expect-error: It should not be any
+          maybeFieldRef satisfies MaybeFieldRefOld<bigint>;
+          expect(maybeFieldRef instanceof MaybeFieldRefOld).toBe(true);
+          expect(maybeFieldRef.get()).toBeUndefined();
+        });
+
+        it("resolves proper field for deep nested items", () => {
+          const field = new FieldOld<string[][][] | undefined>([[["a"]]]);
+          const fieldRef = new FieldRefOld(field);
+          const maybeFieldRef = fieldRef.maybe().at(0).at(0).at(0);
+          maybeFieldRef satisfies MaybeFieldRefOld<string | undefined>;
+          // @ts-expect-error: It should not be any
+          maybeFieldRef satisfies MaybeFieldRefOld<bigint>;
+          expect(maybeFieldRef instanceof MaybeFieldRefOld).toBe(true);
+          expect(maybeFieldRef.get()).toBe("a");
+        });
+      });
+
+      describe("instance", () => {
+        it("returns the undefined field as is if it's defined", () => {
+          const field = new FieldOld<Set<string> | undefined>(undefined);
+          const fieldRef = new FieldRefOld(field);
+          const maybeFieldRef = fieldRef.maybe();
+          maybeFieldRef satisfies MaybeFieldRefOld<Set<string> | undefined>;
+          // @ts-expect-error: It should not be any
+          maybeFieldRef satisfies MaybeFieldRefOld<bigint>;
+          expect(maybeFieldRef instanceof MaybeFieldRefOld).toBe(true);
+          expect(maybeFieldRef.get()).toBeUndefined();
+        });
+
+        it.todo("does not allow to access items by key", () => {
+          const field = new FieldOld<Set<string> | undefined>(undefined);
+          const _fieldRef = new FieldRefOld(field);
+          // TODO:
+          // const maybeFieldRef = fieldRef
+          //   .maybe(new Set<string>())
+          //   // @ts-expect-error: It should not be available
+          //   .maybe("has", (val: string) => false);
+          // maybeFieldRef satisfies never;
+          // expect(maybeFieldRef instanceof MaybeFieldRef).toBe(true);
+          // expect(() => maybeFieldRef.at("a")).toThrowError(
+          //   "Cannot access items of a Set by key"
+          // );
+        });
+      });
+    });
+  });
+
+  describe("#addError", () => {
+    it("adds errors to present fields", () => {
+      const field = new FieldOld<string | number | undefined>("hello");
+      const fieldRef = new FieldRefOld(field);
+      const maybeFieldRef = fieldRef.maybe();
+      maybeFieldRef.addError("Something went wrong");
+      expect(field.errors).toEqual([{ message: "Something went wrong" }]);
+    });
+
+    it("adds errors to undefined fields", () => {
+      const field = new FieldOld<{ hello?: string }>({});
+      const fieldRef = new FieldRefOld(field);
+      const maybeFieldRef = fieldRef.maybe().at("hello");
+      maybeFieldRef.addError("Something went wrong");
+      expect(field.$.hello.errors).toEqual([
+        { message: "Something went wrong" },
+      ]);
+    });
+
+    it("adds errors to shadow fields", () => {
+      const field = new FieldOld<{ hello?: { world?: string } }>({});
+      const fieldRef = new FieldRefOld(field);
+      const maybeFieldRef = fieldRef.maybe().at("hello").at("world");
+      maybeFieldRef.addError("Something went wrong");
+      expect(field.valid).toBe(false);
+      const pavedField = field.$.hello.pave({}).$.world;
+      expect(pavedField.errors).toEqual([{ message: "Something went wrong" }]);
+    });
+
+    it("allows to clear shadow fields errors", () => {
+      const field = new FieldOld<{ hello?: { world?: string } }>({});
+      const fieldRef = new FieldRefOld(field);
+      const maybeFieldRef = fieldRef.maybe().at("hello").at("world");
+      maybeFieldRef.addError("Something went wrong");
+      expect(field.valid).toBe(false);
+      const pavedField = field.$.hello.pave({}).$.world;
+      expect(pavedField.errors).toEqual([{ message: "Something went wrong" }]);
+      field.clearErrors();
+      expect(pavedField.errors).toEqual([]);
+    });
+
+    describe("changes", () => {
+      it("causes target field trigger", async () => {
+        const field = new FieldOld<string | number | undefined>("hello");
+        const fieldRef = new FieldRefOld(field);
+        const maybeFieldRef = fieldRef.maybe();
+        const spy = vi.fn();
+        field.watch(spy);
+        maybeFieldRef.addError("Something went wrong");
+        await postpone();
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(spy).toReceiveChanges(
+          change.field.errors | change.field.invalid,
+        );
+      });
+
+      it("trigger event on the closest target", async () => {
+        const field = new FieldOld<{ name?: { first?: string } }>({});
+        const fieldRef = new FieldRefOld(field);
+        const maybeFieldRef = fieldRef.maybe().at("name").at("first");
+        const spy = vi.fn();
+        field.$.name.watch(spy);
+        maybeFieldRef.addError("Something went wrong");
+        await postpone();
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(spy).toReceiveChanges(
+          change.child.errors | change.child.invalid,
+        );
+      });
+    });
+  });
+});
--- src/form/tests.ts
+++ src/form/tests.ts
@@ -1,74 +1,75 @@
 import { nanoid } from "nanoid";
 import { describe, expect, it, vi } from "vitest";
-import { FieldOld } from "../field/old.tsx";
-import { FormOld } from "./old.tsx";
+import { Form } from "./index.js";
+import { Field } from "../field/index.js";
+import { FieldImpl } from "../field/implementation.ts";
 
-describe.skip("Form", () => {
+describe("Form", () => {
   it("creates a form instance", () => {
-    const form = new FormOld(42);
-    expect(form.get()).toBe(42);
+    const form = new Form(42);
+    expect(form.value).toBe(42);
   });
 
   describe("attributes", () => {
     it("accepts id", () => {
       const id = nanoid();
-      const form = new FormOld(42, { id });
+      const form = new Form(42, { id });
       expect(form.id).toBe(id);
     });
 
     it("returns the internal field", () => {
-      const form = new FormOld(42);
+      const form = new Form(42);
       const { field } = form;
-      expect(field.get()).toBe(42);
-      expect(field).toBeInstanceOf(FieldOld);
+      expect(field.value).toBe(42);
+      expect(field).toBeInstanceOf(Field);
     });
   });
 
   describe("value", () => {
     it("delegates set", () => {
-      const spy = vi.spyOn(FieldOld.prototype, "set");
-      const form = new FormOld(42);
+      const spy = vi.spyOn(FieldImpl.prototype, "set");
+      const form = new Form(42);
       expect(form.set(24)).toBe(form.field);
       expect(spy).toHaveBeenCalledWith(24);
     });
 
     it("delegates initial", () => {
       const spy = vi
-        .spyOn(FieldOld.prototype, "initial", "get")
+        .spyOn(FieldImpl.prototype, "initial", "get")
         .mockReturnValue(123);
-      const form = new FormOld(42);
+      const form = new Form(42);
       expect(form.initial).toBe(123);
       expect(spy).toHaveBeenCalled();
     });
 
     it("delegates dirty", () => {
       const spy = vi
-        .spyOn(FieldOld.prototype, "dirty", "get")
+        .spyOn(FieldImpl.prototype, "dirty", "get")
         .mockReturnValue(true);
-      const form = new FormOld(42);
+      const form = new Form(42);
       expect(form.dirty).toBe(true);
       expect(spy).toHaveBeenCalled();
     });
 
     it("delegates useDirty", () => {
       const spy = vi
-        .spyOn(FieldOld.prototype, "useDirty")
+        .spyOn(FieldImpl.prototype, "useDirty")
         .mockReturnValue(false);
-      const form = new FormOld(42);
+      const form = new Form(42);
       expect(form.useDirty()).toBe(false);
       expect(spy).toHaveBeenCalled();
     });
 
     it("delegates commit", () => {
-      const spy = vi.spyOn(FieldOld.prototype, "commit").mockReturnValue();
-      const form = new FormOld(42);
+      const spy = vi.spyOn(FieldImpl.prototype, "commit").mockReturnValue();
+      const form = new Form(42);
       expect(form.commit()).toBe(undefined);
       expect(spy).toHaveBeenCalled();
     });
 
     it("delegates reset", () => {
-      const spy = vi.spyOn(FieldOld.prototype, "reset").mockReturnValue();
-      const form = new FormOld(42);
+      const spy = vi.spyOn(FieldImpl.prototype, "reset").mockReturnValue();
+      const form = new Form(42);
       expect(form.reset()).toBe(undefined);
       expect(spy).toHaveBeenCalled();
     });
@@ -77,18 +78,18 @@ describe.skip("Form", () => {
   describe("tree", () => {
     it("delegates $", () => {
       const spy = vi
-        .spyOn(FieldOld.prototype, "$", "get")
+        .spyOn(FieldImpl.prototype, "$", "get")
         .mockReturnValue("Ok" as any);
-      const form = new FormOld(42);
+      const form = new Form(42);
       expect(form.$).toBe("Ok");
       expect(spy).toHaveBeenCalled();
     });
 
     it("delegates at", () => {
       const spy = vi
-        .spyOn(FieldOld.prototype, "at")
+        .spyOn(FieldImpl.prototype, "at")
         .mockReturnValue("Ok" as any);
-      const form = new FormOld<string[]>([]);
+      const form = new Form<string[]>([]);
       expect(form.at(5)).toBe("Ok");
       expect(spy).toHaveBeenCalledWith(5);
     });
@@ -98,19 +99,19 @@ describe.skip("Form", () => {
     it("delegates watch", () => {
       const unwatch = () => {};
       const spy = vi
-        .spyOn(FieldOld.prototype, "watch")
+        .spyOn(FieldImpl.prototype, "watch")
         .mockReturnValue(unwatch);
-      const form = new FormOld(42);
+      const form = new Form(42);
       const watchCb = () => {};
       expect(form.watch(watchCb)).toBe(unwatch);
       expect(spy).toHaveBeenCalledWith(watchCb);
     });
 
     it("delegates useWatch", () => {
-      const spy = vi.spyOn(FieldOld.prototype, "useWatch").mockReturnValue();
-      const form = new FormOld(42);
+      const spy = vi.spyOn(FieldImpl.prototype, "useWatch").mockReturnValue();
+      const form = new Form(42);
       const watchCb = () => {};
-      expect(form.useWatch(watchCb)).toBe(undefined);
+      expect(form.useWatch(watchCb, [])).toBe(undefined);
       expect(spy).toHaveBeenCalledWith(watchCb);
     });
   });
@@ -119,9 +120,9 @@ describe.skip("Form", () => {
     it("delegates useCompute", () => {
       const compute = {};
       const spy = vi
-        .spyOn(FieldOld.prototype, "useCompute")
+        .spyOn(FieldImpl.prototype, "useCompute")
         .mockReturnValue(compute as any);
-      const form = new FormOld(42);
+      const form = new Form(42);
       const computeCb = () => {};
       expect(form.useCompute(computeCb as any, [1, 2, 3])).toBe(compute);
       expect(spy).toHaveBeenCalledWith(computeCb, [1, 2, 3]);
@@ -130,9 +131,9 @@ describe.skip("Form", () => {
     it("delegates into", () => {
       const into = {};
       const spy = vi
-        .spyOn(FieldOld.prototype, "into")
+        .spyOn(FieldImpl.prototype, "into")
         .mockReturnValue(into as any);
-      const form = new FormOld({ type: "hello", value: 42 });
+      const form = new Form({ type: "hello", value: 42 });
       const intoCb = () => {};
       expect(form.into(intoCb)).toBe(into);
       expect(spy).toHaveBeenCalledWith(intoCb);
@@ -141,43 +142,21 @@ describe.skip("Form", () => {
     it("delegates useInto", () => {
       const into = {};
       const spy = vi
-        .spyOn(FieldOld.prototype, "useInto")
+        .spyOn(FieldImpl.prototype, "useInto")
         .mockReturnValue(into as any);
-      const form = new FormOld({ type: "hello", value: 42 });
-      const intoCb = () => {};
+      const form = new Form({ type: "hello", value: 42 });
+      const intoCb = (() => {}) as any;
       expect(form.useInto(intoCb, [1, 2, 3])).toBe(into);
       expect(spy).toHaveBeenCalledWith(intoCb, [1, 2, 3]);
-    });
-
-    it("delegates narrow", () => {
-      const narrow = {};
-      const spy = vi
-        .spyOn(FieldOld.prototype, "narrow")
-        .mockReturnValue(narrow as any);
-      const form = new FormOld({ type: "hello", value: 42 });
-      const narrowCb = () => {};
-      expect(form.narrow(narrowCb as any)).toBe(narrow);
-      expect(spy).toHaveBeenCalledWith(narrowCb);
-    });
-
-    it("delegates useNarrow", () => {
-      const narrow = {};
-      const spy = vi
-        .spyOn(FieldOld.prototype, "useNarrow")
-        .mockReturnValue(narrow as any);
-      const form = new FormOld({ type: "hello", value: 42 });
-      const narrowCb = () => {};
-      expect(form.useNarrow(narrowCb as any, [1, 2, 3])).toBe(narrow);
-      expect(spy).toHaveBeenCalledWith(narrowCb, [1, 2, 3]);
     });
   });
 
   describe("errors", () => {
     it("delegates valid", () => {
       const spy = vi
-        .spyOn(FieldOld.prototype, "valid", "get")
+        .spyOn(FieldImpl.prototype, "valid", "get")
         .mockReturnValue(false);
-      const form = new FormOld(42);
+      const form = new Form(42);
       expect(form.valid).toBe(false);
       expect(spy).toHaveBeenCalled();
     });
@@ -186,20 +165,20 @@ describe.skip("Form", () => {
   describe("validation", () => {
     it("delegates validate", async () => {
       const validateSpy = vi
-        .spyOn(FieldOld.prototype, "validate")
+        .spyOn(FieldImpl.prototype, "validate")
         .mockReturnValue(Promise.resolve(void 0));
       const validSpy = vi
-        .spyOn(FieldOld.prototype, "valid", "get")
+        .spyOn(FieldImpl.prototype, "valid", "get")
         .mockReturnValue(false);
       const validateCb = () => {};
-      const form = new FormOld(42, { validate: validateCb });
+      const form = new Form(42, { validate: validateCb });
       expect(await form.validate()).toBe(false);
       expect(validateSpy).toHaveBeenCalledWith(validateCb);
       expect(validSpy).toHaveBeenCalled();
     });
 
     it("clears errors even if the validate function is not provided", async () => {
-      const form = new FormOld(42);
+      const form = new Form(42);
       form.field.addError("Nope");
       expect(await form.validate()).toBe(true);
       expect(form.field.errors).toHaveLength(0);
