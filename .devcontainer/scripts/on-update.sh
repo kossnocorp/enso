b9a474197951169753057e706d2dbe045017c324
Sasha Koss <koss@nocorp.me>
2025-10-31 05:09:20 +0000

    Fix dev container env vars setup

--- .devcontainer/scripts/on-update.sh
+++ .devcontainer/scripts/on-update.sh
@@ -7,9 +7,12 @@ set -e
 source ~/.config/mothership/.env || true
 
 # Make sure mise is activated
+
 if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
   export PATH="$HOME/.local/bin:$PATH"
 fi
+# Trust it
+mise trust --yes --all
 # Update it
 mise self-update -y
 # Activate it
@@ -17,33 +20,33 @@ eval "$(mise activate bash --shims)"
 eval "$(mise env -s bash)"
 
 # Pull git submodules
+
 if [ -e .git ]; then
   git submodule update --recursive --init --remote
 fi
-
-# Trust all mise configs
-mise trust --yes --all
+# Trust all submodule mise configs
 if [ -d .git ]; then
   git submodule foreach --recursive "mise trust"
 fi
 
 # Install stack
+
 mise install
-mise reshim
 
 # Install dependencies
-corepack enable || true # Try enabling corepack
+
+# Node.js
 if [ -f ./pnpm-lock.yaml ]; then
-  yes | pnpm install
+  yes | mise x pnpm -- pnpm install || echo -e "🟡 pnpm install failed, make sure to run:\n\n    pnpm install\n"
 elif [ -f ./yarn.lock ]; then
-  yes | yarn install
+  yes | mise x yarn -- yarn install || echo -e "🟡 yarn install failed, make sure to run:\n\n    yarn install\n"
 elif [ -f ./package-lock.json ]; then
-  yes | npm install
+  yes | mise x node -- npm install || echo -e "🟡 npm install failed, make sure to run:\n\n    npm install\n"
 fi
 
 # Install dependencies
 if [ -f ./Cargo.lock ]; then
-  cargo build || echo "🟡 Cargo build failed, but that's ok"
+  mise x rust -- cargo build || echo -e "🟡 Cargo build failed, make sure to run:\n\n    cargo build\n"
 fi
 
 # NOTE: When needed, run setup scripts here.
